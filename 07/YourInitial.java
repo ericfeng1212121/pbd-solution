11ea8c8669f7accded3701c1689290978d6455ec
EricTheLittlePony123 <36411295+ericfeng1212121@users.noreply.github.com>
2018-02-21 09:44:08 -0500

    pbd-solution 07

---
+++ 07/YourInitial.java
@@ -0,0 +1,13 @@
+public class YourInitial
+{
+		public static void main( String[]args )
+		{
+			System.out.println( "EEEEE  FFFFF" );
+			System.out.println( "E      F    " );
+			System.out.println( "E      F    " );
+			System.out.println( "EEE    FFF  " );
+			System.out.println( "E      F    " );
+			System.out.println( "E      F    " );
+			System.out.println( "EEEEE  F    " );
+		}
+}
