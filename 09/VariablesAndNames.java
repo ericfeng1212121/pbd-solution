820ec8b3a6d757106c2e85b80a6d3510d49e352e
EricTheLittlePony123 <36411295+ericfeng1212121@users.noreply.github.com>
2018-02-21 09:44:52 -0500

    pbd-solution 09

---
+++ 09/VariablesAndNames.java
@@ -0,0 +1,9 @@
+public class VariablesAndNames{
+    public static void main(String[] args){
+		int student;
+		String school;
+		student = 1700;
+		school = "St.Robert CHS";
+		System.out.println("There are " + student + " students in " + school + ".");
+	}
+}
